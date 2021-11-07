098733ce629f1dca1e7cb01ac8c9c1d37b9e2dcd
Tanya <tanyach1997@gmail.com>
2021-11-07 14:27:54 +0530

    Create discriminator_utils.py

---
+++ pix2pix/utils/discriminator_utils.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+from pix2pix.utils.utils import Reflection_Pad
+
+class Conv_Downsample_Block(tf.keras.layers.Layer):
+    def __init__(self,filters,stride,bn):
+        super(Conv_Downsample_Block,self).__init__()
+        self.filters = filters
+        self.stride = stride
+        self.bn = bn
+        self.conv_layer = tf.keras.layers.Conv2D(self.filters, 
+                                                 4, 
+                                                 self.stride,
+                                                 padding='valid')
+        self.bn_layer = tf.keras.layers.BatchNormalization(momentum=0.8)
+        self.lrelu_layer = tf.keras.layers.LeakyReLU(alpha=0.2)
+        self.reflection_pad = Reflection_Pad(pad=(1,1))
+
+
+    def call(self,input_tensor,training=None):
+        x = self.reflection_pad(input_tensor)
+        x = self.conv_layer(input_tensor)
+        if self.bn:
+            x = self.bn_layer(x, training=training)
+        x = self.lrelu_layer(x)
+        return x
