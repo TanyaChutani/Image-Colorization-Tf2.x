c6db70da64ddde039fd1422645c676418f67a9e0
Tanya <tanyach1997@gmail.com>
2021-11-17 02:55:56 +0530

    Update generator.py

--- pix2pix/model/generator.py
+++ pix2pix/model/generator.py
@@ -17,27 +17,27 @@ class Generator(tf.keras.layers.Layer):
         self.tanh = tanh
         self.dropout = dropout 
         self.model_output =  Conv_Decoder_Block(3,
-                                            False,
-                                            self.dropout,
-                                            True)
+                                                False,
+                                                self.dropout,
+                                                True)
     def make_encoder_block(self):
         label = []
         label.append(Conv_Encoder_Block(self.filters, 
-                                       False,
-                                       self.relu))
+                                        False,
+                                        self.relu))
         for _ in range(2):
             self.filters = self.filters * 2
             label.append(Conv_Encoder_Block(self.filters,
-                                              self.bn,
-                                              self.relu))
+                                            self.bn,
+                                            self.relu))
         self.filters = self.filters * 2
         for _ in range(4):
             label.append(Conv_Encoder_Block(self.filters,
-                                              self.bn,
-                                              self.relu))
+                                            self.bn,
+                                            self.relu))
         label.append(Conv_Encoder_Block(self.filters,
-                                           self.bn, 
-                                           True))
+                                        self.bn, 
+                                        True))
         return label
     
     def make_decoder_block(self):
