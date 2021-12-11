816ad308578fed63bdb348cb80e440e4c550bec1
Tanya <tanyach1997@gmail.com>
2021-12-11 23:12:39 +0530

    Update utils.py

--- pix2pix/utils/utils.py
+++ pix2pix/utils/utils.py
@@ -22,25 +22,3 @@ def separate_image(path):
     input_img = image[:, input_w:, :]
     target_img = image[:, :input_w, :]
     return input_img, target_img
-    
-def preprocess(image, resize_width=286, resize_height=286, image_width=256, image_height=256, mode="train"):
-    if mode == "train":
-        image = tf.image.resize(image,size=(resize_width, resize_height),
-                                   method=tf.image.ResizeMethod.BILINEAR)
-        image = tf.image.random_crop(image,size=(image_width,image_height,3))
-    image = image/127.5 - 1
-    image = tf.cast(image,tf.float32)
-    return image
-    
-
-def train_preprocess_image(path):
-    input_image, target_image = separate_image(path)
-    input_image = preprocess(input_image)
-    target_image = preprocess(target_image)
-    return input_image, target_image
-
-def test_preprocess_image(path):
-    input_image, target_image = separate_image(path)
-    input_image = preprocess(input_image, mode="test")
-    target_image = preprocess(target_image, mode="test")
-    return input_image, target_image
