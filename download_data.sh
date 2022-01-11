ce85698d8db044ff710c2f82d86130b93062e15d
Tanya <tanyach1997@gmail.com>
2022-01-12 04:42:15 +0530

    Update download_data.sh

--- download_data.sh
+++ download_data.sh
@@ -1,2 +1,5 @@
-curl -O http://efrosgans.eecs.berkeley.edu/pix2pix/datasets/cityscapes.tar.gz
-tar -xf cityscapes.tar.gz
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_train_HR.zip
+wget http://data.vision.ee.ethz.ch/cvl/DIV2K/DIV2K_valid_HR.zip
+
+jar xvf  DIV2K_train_HR.zip
+jar xvf DIV2K_valid_HR.zip
