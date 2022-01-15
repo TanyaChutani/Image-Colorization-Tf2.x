293de5d004d9cec344a6b5b732ad136cb9824b53
Tanya <tanyach1997@gmail.com>
2022-01-16 04:51:48 +0530

    Update discriminator_loss.py

--- pix2pix/loss/discriminator_loss.py
+++ pix2pix/loss/discriminator_loss.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 
+
 class Discriminator_Loss(tf.keras.losses.Loss):
-    def __init__(self,**kwargs):
-        super(Discriminator_Loss,self).__init__()
-        self.mse_loss = tf.keras.losses.MeanSquaredError()
+    def __init__(self, **kwargs):
+        super(Discriminator_Loss, self).__init__()
+        self.bce_loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 
     def call(self, disc_true, disc_op):
-        real_loss = self.mse_loss(disc_true, tf.ones_like(disc_true))
-        fake_loss = self.mse_loss(disc_op, tf.zeros_like(disc_op))
+        real_loss = self.bce_loss(disc_true, tf.ones_like(disc_true))
+        fake_loss = self.bce_loss(disc_op, tf.zeros_like(disc_op))
         final_disc_loss = real_loss + fake_loss
         return final_disc_loss
-    
