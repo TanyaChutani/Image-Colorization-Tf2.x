d2be82c5cd18ca04fcfef02b359f7f8434876534
Tanya <tanyach1997@gmail.com>
2022-01-20 00:20:25 +0530

    Update pix2pix.py

--- pix2pix/model/pix2pix.py
+++ pix2pix/model/pix2pix.py
@@ -7,53 +7,67 @@ from pix2pix.loss.generator_loss import Generator_Loss
 
 
 class PIX2PIX(tf.keras.models.Model):
-    def __init__(self,
-                 var_lambda=100,
-                 **kwargs):
+    def __init__(self, var_lambda=100.0, **kwargs):
         super(PIX2PIX, self).__init__()
+        self.var_lambda = var_lambda
+
         self.generator_model = Generator()
         self.discriminator_model = Discriminator()
         self.gen_loss = Generator_Loss()
         self.disc_loss = Discriminator_Loss()
-        self.var_lambda = var_lambda
-        
-    def compile(self, generator_optimizer, discriminator_optimizer, loss):
+
+    def compile(self, generator_optimizer, discriminator_optimizer, metric):
         super(PIX2PIX, self).compile()
+        self.metric = metric
         self.generator_optimizer = generator_optimizer
         self.discriminator_optimizer = discriminator_optimizer
-        self.l1_loss = loss
 
     def train_step(self, data):
+
         input_image, output_image = data
 
         with tf.GradientTape() as discriminator_tape, tf.GradientTape() as generator_tape:
+
             gen_output = self.generator_model(input_image, training=True)
-            disc_real_output = self.discriminator_model(input_image, output_image, training=True)
-            disc_fake_output = self.discriminator_model(input_image, gen_output, training=True)
-            
-            generator_pixel_loss = self.l1_loss(gen_output, output_image)
+            disc_real_output = self.discriminator_model(
+                input_image, output_image, training=True
+            )
+            disc_fake_output = self.discriminator_model(
+                input_image, gen_output, training=True
+            )
 
-            generator_loss = self.gen_loss(disc_fake_output,
-                                           gen_output)
+            generator_pixel_loss = tf.reduce_mean(tf.abs(output_image - gen_output))
+            generator_loss = tf.reduce_mean(
+                self.gen_loss(disc_fake_output, disc_fake_output)
+            )
+            final_gen_loss = generator_loss + (self.var_lambda * generator_pixel_loss)
 
-            final_gen_loss = generator_loss + self.var_lambda * generator_pixel_loss    
+            discriminator_loss = tf.reduce_mean(
+                self.disc_loss(disc_real_output, disc_fake_output)
+            )
 
-            discriminator_loss = self.disc_loss(disc_real_output, disc_fake_output)
+            psnr_metric = self.metric(output_image, gen_output)
 
+        generator_gradients = generator_tape.gradient(
+            final_gen_loss, self.generator_model.trainable_variables
+        )
 
-        discriminator_gradients = discriminator_tape.gradient(discriminator_loss,
-                                                              self.discriminator_model.trainable_variables)
-        generator_gradients = generator_tape.gradient(generator_loss, self.generator_model.trainable_variables)
+        discriminator_gradients = discriminator_tape.gradient(
+            discriminator_loss, self.discriminator_model.trainable_variables
+        )
 
         self.discriminator_optimizer.apply_gradients(
-            zip(discriminator_gradients, self.discriminator_model.trainable_variables))
-        self.generator_optimizer.apply_gradients(zip(generator_gradients, self.generator_model.trainable_variables))
+            zip(discriminator_gradients, self.discriminator_model.trainable_variables)
+        )
+        self.generator_optimizer.apply_gradients(
+            zip(generator_gradients, self.generator_model.trainable_variables)
+        )
+        return {
+            "generator_loss": final_gen_loss,
+            "discriminator_loss": discriminator_loss,
+            "psnr_metric": psnr_metric,
+        }
 
-        return {"generator_loss": final_gen_loss,
-                "discriminator_loss": discriminator_loss}
-    
-     
     def call(self, input_image):
         op = self.generator_model(input_image, training=False)
         return op
-
