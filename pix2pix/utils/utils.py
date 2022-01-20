c1749cc3f804de3e305f195255d212da648dc5c4
Tanya <tanyach1997@gmail.com>
2022-01-21 00:09:46 +0530

    removed unnecessary code

--- pix2pix/utils/utils.py
+++ pix2pix/utils/utils.py
@@ -13,12 +13,3 @@ class Reflection_Pad(tf.keras.layers.Layer):
                        [pad_width, pad_height],
                        [0, 0]],
                       mode = "REFLECT")
-
-def separate_image(path):
-    image = tf.io.read_file(path)
-    image = tf.image.decode_jpeg(image, channels=3)
-    original_w = tf.shape(image)[1]
-    input_w = original_w // 2
-    input_img = image[:, input_w:, :]
-    target_img = image[:, :input_w, :]
-    return input_img, target_img
